95c81c9d4b64118558b34368d943f8572c2ad24b
Evan Cillie <evancillie@Evans-MacBook-Air-3.local>
2025-03-21 14:15:52 -0700

    adding backend:

---
+++ backend/src/main/java/com/airportWebApp/airport_web_app/AirportWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.airportWebApp.airport_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportWebAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/airportWebApp/airport_web_app/AirportWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airportWebApp.airport_web_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
