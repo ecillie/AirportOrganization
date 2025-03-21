e46570f66871290a37e0650a618ebe663e3d240b
Evan Cillie <evancillie@Evans-MacBook-Air-3.local>
2025-03-21 15:12:46 -0700

    backend update

--- backend/src/test/java/com/airportWebApp/airport_web_app/AirportWebAppApplicationTests.java
+++ backend/src/test/java/com/airportWebApp/airport_web_app/AirportWebAppApplicationTests.java
@@ -2,12 +2,14 @@ package com.airportWebApp.airport_web_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class AirportWebAppApplicationTests {
+@ActiveProfiles("test")
+public class AirportWebAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // The context should load without exceptions
+    }
 }
